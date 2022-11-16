5ba1c07aaa607d4c4d1c47f6156e568c1a30cacd
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-11-16 12:33:56 +0100

    Fix: fixed mongo error.

--- src/database/db.js
+++ src/database/db.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose');
 
-const db_uri = (process.env.NODE_ENV = "development"
-  ? process.env.MONGO_URI
-  : process.env.MONGO_URI_PROD); 
+const { MONGO_URI_PROD } = process.env;
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(db_uri);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(MONGO_URI_PROD);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
