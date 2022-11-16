e0ef3790a2ee05a75005866fecf19a987ae0ef88
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-11-16 12:27:41 +0100

    Deploy: Pushing to heroku

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
 });
     
 
